11ca2c1c849d161e8d5830b348d319389d48aabd
arunkpskpm <arunkpskpm@gmail.com>
2017-03-24 15:18:41 +0800

    Update setup_data_collection.sh
    Added few more commands

--- setup_data_collection.sh
+++ setup_data_collection.sh
@@ -4,7 +4,7 @@
 # NAME: setup_data_collection.sh
 # PURPOSE: This script will prepare a "new" script /var/tmp/srvmon/perf_monitor.sh on Linux/Solaris servers. New script will be added to crontab, scheduled to run every minutes.
 # Performance logs will be saved under /var/tmp/srvmon. Log rotation will be enabled to rotate log files weekly (will keep 6 older versions of logs)
-# AUTHOR: arunkpskpm@gmil.com
+# AUTHOR: arun.kp@hpe.com
 # DATE WRITTEN: 21 July 2016
 # MODIFICATION HISTORY: 21 July 2016 - Initial release
 # MODIFICATION HISTORY: 20 March 2017 - Added Solaris log rotation 
@@ -47,15 +47,15 @@ cat > /var/tmp/srvmon/perf_monitor.sh << "End-of-message"
 #========================================================================
 # NAME: perf_monitor.sh
 # PURPOSE: Script to collect performance data from the server
-# AUTHOR: arunkpskpm@gmil.com
+# AUTHOR: arun.kp@hpe.com
 # DATE WRITTEN: 06 August 2015
 # MODIFICATION HISTORY: 06 Aug 2015 - Initial release
 #                       31 Oct 2016 - Added sar,pidstat commands
 #========================================================================
 #Defining commands to use
 DATE=`date`
-TOPCPU=`top -d 5 -n 1 -b | head -30 |sed 's/%/%%/g'`           #Top process utilization sorted based on CPU usage
-TOPMEM=`top -a -d 5 -n 1 -b | head -30 |sed 's/%/%%/g'`        #Top process utilization sorted based on Memory usage
+TOPCPU=`top -d 5 -n 1 -b | head -30 |sed 's/%/%%/g'`           #top sorted based on CPU usage
+TOPMEM=`top -a -d 5 -n 1 -b | head -30 |sed 's/%/%%/g'`        #top sorted based on Memory usage
 SWAP=` free | grep -i swap | tr -d [A-z],\:,\+,\=,\-,\/, | awk '{print"Swap free: "($3)/($1)*100"%"}' |sed 's/%/%%/g'`
 VMSTAT=`vmstat 1 10`
 IOSTAT=`iostat -xdmtc 1 5 |sed 's/%/%%/g' `
@@ -91,7 +91,7 @@ echo "* * * * * /var/tmp/srvmon/perf_monitor.sh  >/dev/null 2>&1 " >> /var/spool
 check_error
 #Setting up log rotation 
 cat > /etc/logrotate.d/srvmon  << "End-of-message"
-/var/tmp/srvmon/top.log /var/tmp/srvmon/swap.log /var/tmp/srvmon/vmstat.log /var/tmp/srvmon/iostat.log /var/tmp/srvmon/pidstat.log /var/tmp/srvmon/sar_cpu.log /var/tmp/srvmon/sar_mem.log
+/var/tmp/srvmon/top_cpu.log /var/tmp/srvmon/top_mem.log /var/tmp/srvmon/swap.log /var/tmp/srvmon/vmstat.log /var/tmp/srvmon/iostat.log /var/tmp/srvmon/pidstat.log /var/tmp/srvmon/sar_cpu.log /var/tmp/srvmon/sar_mem.log
 {
     compress
     weekly
@@ -112,23 +112,25 @@ cat > /var/tmp/srvmon/perf_monitor.sh  << "End-of-message"
 #========================================================================
 # NAME: perf_monitor.sh
 # PURPOSE: Script to collect real-time system performance data from a Solaris server
-# AUTHOR: arunkpskpm@gmil.com
+# AUTHOR: arun.kp@hpe.com
 # DATE WRITTEN: 06 August 2015
 # MODIFICATION HISTORY: 06 August 2015 - Initial release
 #========================================================================
 #Defining variables
 DATE=`date`
-PRSTAT=`prstat -s cpu  -can 100 1 1|sed 's/%/%%/g'`		# prstat sorted based on cpu utilization. Enable either of it based on situation.
-#PRSTAT=`prstat -s rss  -can 100 1 1|sed 's/%/%%/g'` 		# prstat sorted based on memory utilization. Enable either of it based on situation.
+PRSTATCPU=`prstat -s cpu  -can 50 1 1|sed 's/%/%%/g'`		# prstat sorted based on cpu utilization. 
+PRSTATMEM=`prstat -s rss  -can 50 1 1|sed 's/%/%%/g'` 		# prstat sorted based on memory utilization.  
 SWAP=`swap -s | awk ' { print $9 $11 } ' | awk -Fk '{print $1 * 100 / ( $1 + $2 ) }'`
 VMSTAT=`vmstat 1 5`
 IOSTAT=`iostat -xnmpz 2 5 |tail -200|sed 's/%/%%/g'`
-LOG_PRSTAT="/var/tmp/srvmon/prstat.log"
+LOG_PRSTATCPU="/var/tmp/srvmon/prstat_cpu.log"
+LOG_PRSTATMEM="/var/tmp/srvmon/prstat_mem.log"
 LOG_SWAP="/var/tmp/srvmon/swap.log"
 LOG_VMSTAT="/var/tmp/srvmon/vmstat.log"
 LOG_IOSTAT="/var/tmp/srvmon/iostat.log"
 #Writing logs collected
-printf "\n $DATE \n $PRSTAT \n " >>$LOG_PRSTAT
+printf "\n $DATE \n $PRSTATCPU \n " >>$LOG_PRSTATCPU
+printf "\n $DATE \n $PRSTATMEM \n " >>$LOG_PRSTATMEM
 printf "\n $DATE \n $SWAP \n " >>$LOG_SWAP
 printf "\n $DATE \n $VMSTAT \n " >>$LOG_VMSTAT
 printf "\n $DATE \n $IOSTAT \n " >>$LOG_IOSTAT
@@ -147,7 +149,8 @@ svcadm restart cron
 printf "\n$CYAN Please wait while running script for first time... \n $RESET"
 /var/tmp/srvmon/perf_monitor.sh 
 #Setting up log rotation 
-logadm -w /var/tmp/srvmon/prstat.log -C 6 -p 1w -c -z 0
+logadm -w /var/tmp/srvmon/prstat_cpu.log -C 6 -p 1w -c -z 0
+logadm -w /var/tmp/srvmon/prstat_mem.log -C 6 -p 1w -c -z 0
 logadm -w /var/tmp/srvmon/swap.log -C 6 -p 1w -c -z 0
 logadm -w /var/tmp/srvmon/vmstat.log -C 6 -p 1w -c -z 0
 logadm -w /var/tmp/srvmon/iostat.log -C 6 -p 1w -c -z 0
